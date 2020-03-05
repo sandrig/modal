96935cd96aa413c9b6d9307c767036ddba97795f
sandrig <sandrig.dev@gmail.com>
2020-03-05 22:42:15 +0300

    feat: add footer buttons

--- index.js
+++ index.js
@@ -11,5 +11,15 @@ const modal = $.modal({
     <p>Lorem ipsum dolor sit.</p>
     <p>Lorem ipsum dolor sit.</p>
   `,
-  width: '400px'
+  width: '400px',
+  footerButtons: [
+    { text: 'Ok', type: 'primary', handler() {
+      console.log('Primary btn clicked')
+      modal.close()
+    }},
+    { text: 'Cancel', type: 'danger', handler() {
+      console.log('Danger btn clicked')
+      modal.close()
+    }},
+  ]
 })
